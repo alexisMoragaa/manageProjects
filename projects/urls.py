c770b57dbc00ad12b44a5d18d55e138e52f0d5c1
Alexis Moraga <=>
2021-05-17 02:02:09 -0400

    add view projects, add view project, start to develop drag and drop

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,7 @@ from .import views
 
 urlpatterns = [
     path('', views.home, name='Home'),
-    path('index', views.index, name='Index')
+    path('index', views.index, name='Index'),
+    path('projects', views.projects, name='Projects'),
+    path('project/id/<int:id>', views.project, name='Project')
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,4 @@
+from projects.models import Column, Project
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -7,5 +8,16 @@ def index(request):
 
 
 def home(request):
-    
     return render(request,'Home.html')
+
+
+def projects(request):
+    """Return all projects createds in database"""
+    projects = { 'projects' :  Project.objects.all() }
+    return render(request, 'Projects.html', projects)
+
+
+def project(request, id):
+    data = {'project' : Project.objects.get(pk = id), 'columns': Column.objects.filter(project = id)}
+    # return HttpResponse(data.project)
+    return render(request, 'Project.html', data)
